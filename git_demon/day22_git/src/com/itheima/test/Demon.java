e82b094453baa85d04f47f6c5044c9fc59c5af7c
lisi <lisi.itcast>
2019-11-23 20:03:56 +0800

    第二次提交

--- git_demon/day22_git/src/com/itheima/test/Demon.java
+++ git_demon/day22_git/src/com/itheima/test/Demon.java
@@ -3,5 +3,6 @@ package com.itheima.test;
 public class Demon {
     public static void main(String[] args) {
         System.out.println("蓦然回首那人却在灯火阑珊下");
-    }
+        System.out.println("喜欢你");
+}
 }
